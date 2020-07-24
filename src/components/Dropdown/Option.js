ea9db9081fa0bd60935fbbc115d13fcaa05681d8
Laercio Santos <laerciosantosn@gmail.com>
2020-07-24 12:46:13 -0300

    Finalizado o component de DorpdownOption

--- src/components/Dropdown/Option.js
+++ src/components/Dropdown/Option.js
@@ -1,8 +1,90 @@
-import React from 'react';
+import React, { useRef, useState, useContext, useEffect} from 'react';
+import { motion } from 'framer-motion';
 
-export function DropdownOption({name, content: Content}) {
+import { Context } from './Provider';
+import { useDimensions } from './dimensions';
+
+let lastOptionId = 0;
+
+export function DropdownOption({name, content: Content, backgroundHeight}) {
+  const idRef = useRef(++lastOptionId);
+  const id = idRef.current;
+
+  const [optionHook, optionDimensions] = useDimensions();
+  const [registered, setRegistered] = useState(false);
+
+  const {
+    registerOption,
+    updateOptionProps,
+    deleteOptionByid,
+    setTargetId,
+    targetId,
+  } = useContext(Context);
+
+  useEffect(() => {
+    if (!registered && optionDimensions) {
+      const WrapperContent = () => {
+        const contentRef = useRef();
+
+        useEffect(() => {
+         const contentDimensioins = contentRef.current.getBoundingClientRect();
+         updateOptionProps(id, { contentDimensioins })
+        }, [])
+
+        return (
+          <div ref={contentRef}>
+            <Content />
+          </div>
+        )
+      }
+
+      registerOption({
+        id,
+        optionDimensions,
+        optionCenterX: optionDimensions.x + optionDimensions.width / 2,
+        WrapperContent,
+        backgroundHeight,
+      })
+      setRegistered(true)
+    } else if (registered && optionDimensions) {
+      updateOptionProps(id, {
+        optionDimensions,
+        optionCenterX: optionDimensions.x + optionDimensions.width / 2,
+      })
+    }
+  }, [
+      registerOption, 
+      id, 
+      registered, 
+      optionDimensions,
+      updateOptionProps, 
+      deleteOptionByid, 
+      backgroundHeight, 
+  ]);
+
+  const handleOpen = () => setTargetId(id);
+  const handleClose = () => setTargetId(null);
+  const handleTouch = () => (window.isMobile = true);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    return targetId === id ? handleClose() : handleOpen();
+  };
+
   return (
-    <button className="dorpdown-option">{name}</button>
+    <motion.button 
+      className="dorpdown-option"
+      ref={optionHook}
+      onMouseDown={handleClick}
+      onHoverStart={() => !window.isMoblie && handleOpen()}
+      onHoverEnd={() => !window.isMoblie && handleClose()}
+      onTouchStart={handleTouch}
+      onFocus={handleOpen}
+      onBlur={handleClose}
+    >
+      {name}
+    </motion.button>
 
   )
 }
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,34 +1,37 @@
 import React from 'react';
 
+import { Products, Developers, Company } from '../Content';
 import { Container, DropdownStyles } from './styles';
-import { DropdownOption } from '../Dropdown';
+import { DropdownOption, DropdownProvider } from '../Dropdown';
 
 function NavBar() {
   return (
-    <DropdownStyles> 
-    <Container>
-      < ul>
-        <li>
-          <DropdownOption 
-            name="Produtos"
-            content={() => <h1>Produtos</h1>}
-          />
-        </li>
-        <li>
-          <DropdownOption 
-            name="Desenvolvedores"
-            content={() =>  <h1>Desenvolvedores</h1>}
-          />
-        </li>
-        <li>
-          <DropdownOption 
-            name="Empresa"
-            content={() =>  <h1>Empresa</h1>}
-          />
-        </li>
-      </ul>
-    </Container>
-    </DropdownStyles>
+    <DropdownProvider>
+      <DropdownStyles>
+        <Container>
+          < ul>
+            <li>
+              <DropdownOption
+                name="Produtos"
+                content={() => <h1>{Products}</h1>}
+              />
+            </li>
+            <li>
+              <DropdownOption
+                name="Desenvolvedores"
+                content={() => <h1>{Developers}</h1>}
+              />
+            </li>
+            <li>
+              <DropdownOption
+                name="Empresa"
+                content={() => <h1>{Company}</h1>}
+              />
+            </li>
+          </ul>
+        </Container>
+      </DropdownStyles>
+    </DropdownProvider>
   )
 }
 
